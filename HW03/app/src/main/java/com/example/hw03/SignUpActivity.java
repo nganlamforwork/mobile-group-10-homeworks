003540c99e0bda114aee5548cee2e964f6cc7c9a
Le Vu Ngan Lam <nganlamforwork@gmail.com>
2024-01-27 00:57:43 +0700

    first commit

---
+++ HW03/app/src/main/java/com/example/hw03/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.hw03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
